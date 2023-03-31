e6b669e69158ed7d0d68afc0f2da25ae6e0f77a3
EtienneChamarier <etienne.chamarier@gmail.com>
2023-03-31 10:38:34 +0200

    maj quete2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,9 @@
 function Image() {
-return (
-<figure>
+ return (
+ <figure>
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
     <figcaption>bulbasaur</figcaption>
-</figure>
+ </figure>
 )
 }
 
