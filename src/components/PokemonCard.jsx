7d834277487da593e70ed376bac5b5351a77899d
EtienneChamarier <etienne.chamarier@gmail.com>
2023-03-31 11:14:25 +0200

    ajout quete 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -7,4 +7,30 @@ function Image() {
 )
 }
 
+function PokemonCard() {
+
+const pokemonList = [
+   {
+     name: "bulbasaur",
+     imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+   },
+   {
+     name: "mew", 
+   },
+ ];
+
+ const pokemon = pokemonList[0];
+
+ return (
+   <div>
+      <figure>
+        {imgSrc && <img src={imgSrc} />}
+        {!imgSrc && <p>???</p>}
+        <figcaption>{name}</figcaption>
+      </figure>
+
+   </div>
+ );
+}
+
 export default Image;
